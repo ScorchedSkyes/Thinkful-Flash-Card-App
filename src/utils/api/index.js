b146ad64f79777e9691a16299926841a6f011816
ScorchedSkyes <Kris.harder1929@gmail.com>
2022-12-05 10:48:21 -0600

    Fixed back-end issue

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -4,7 +4,7 @@
  */
  const API_BASE_URL =
  process.env.REACT_APP_API_BASE_URL ||
- "https://rpm-flashcards-server.herokuapp.com";
+ "http://localhost:8080";
 // const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
 
 /**
